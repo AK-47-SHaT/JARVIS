5e785b90cf7cd6c64f413bac719f6189f5f4d187
Akshat Chauhan <akshataaryan511@gmail.com>
2024-12-20 19:43:39 +0530

    added the basic jarvis model

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # JARVIS
 Ek sasta assistant (because i dont have 5$ for OpenAI API ) else it would have been fire 
 <br>
+<br>
---
+++ jarvis.py
@@ -0,0 +1,74 @@
+import webbrowser
+import pyttsx3
+import speech_recognition as sr
+
+
+
+
+r = sr.Recognizer()
+engine=pyttsx3.init()
+
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+
+
+
+
+def process(command):
+    if"open google" in command.lower():
+          webbrowser.open("http:\\google.com")
+
+    elif"shut up" in command.lower():
+        speak("ari pup")      
+    elif"akshat " in command.lower():
+        speak("aap akshat aaryan")    
+    listen()
+     
+
+def takingCommand():
+    try:
+        with sr.Microphone() as source:
+            print("Say something!")
+            audio = r.listen(source)
+        command = r.recognize_google(audio)
+        process(command)
+                                    #speak("I thinks you said " + r.recognize_google(audio))
+    except Exception as e :
+        print(e)
+        speak("could not lisen command")
+        takingCommand()
+
+
+    
+def listen():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+
+
+    # recognize speech using Google Speech Recognition
+    try:
+        if( r.recognize_google(audio).lower()=="jarvis"):
+         speak("ya")
+         print(f"you: {r.recognize_google(audio)}")
+         print("jarvis: ya")
+         takingCommand()
+        
+    except sr.UnknownValueError:
+        print("I could not understand jarvis")
+        listen()
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+        listen()
+
+while(True):
+    listen()        
